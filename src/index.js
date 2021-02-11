ab8924057752a4d36f74e22c9c6e4c19c33ff6c8
suprem.trivedi@paconsulting.com <suprem.trivedi@paconsulting.com>
2021-02-11 22:21:53 +0000

    netlify identity

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
+import CMS from 'netlify-cms-app'
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
