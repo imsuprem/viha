424de35ddd7b8ada5c43409a857fe8f4ae3ae3e3
suprem.trivedi@paconsulting.com <suprem.trivedi@paconsulting.com>
2021-02-11 22:45:45 +0000

    fixed build.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import './index.css';
 import App from './components/App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
-import CMS from 'netlify-cms-app'
-
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
