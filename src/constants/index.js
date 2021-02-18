ab4b1856873af93ced38e8abdd12215a6585726e
suprem.trivedi@paconsulting.com <suprem.trivedi@paconsulting.com>
2021-02-18 23:46:31 +0000

    Updates

--- src/constants/index.js
+++
@@ -1,2 +0,0 @@
-export { default as COUNTDOWN_TIME } from './countdownTime';
-export { default as NUM_OF_QUESTIONS } from './numOfQuestions';
--- src/utils/calculateScore.js
+++
@@ -1,5 +0,0 @@
-const calculateScore = (totalQuestions, correctAnswers) => {
-  return Number(((correctAnswers * 100) / totalQuestions).toFixed(2));
-};
-
-export default calculateScore;
